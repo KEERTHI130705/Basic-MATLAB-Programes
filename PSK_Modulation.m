716c82e21ce354b47477eb0f61657c22ec867609
ALLE KEERTHI HARSHA <130027038+KEERTHI130705@users.noreply.github.com>
2025-05-12 15:41:48 +0530

    Create PSK_Modulation.m

---
+++ PSK_Modulation.m
@@ -0,0 +1,45 @@
+clc;
+clear all;
+close all;
+
+d = input("Enter the digital signal bits [e.g. [1 0 1 1]]: ");
+A = input("Enter the amplitude of Carrier signal: ");
+Fc = input("Enter the frequency of Carrier signal (Hz): ");
+
+Fs = 100 * Fc;          % Sampling frequency
+Tb = 1;                 % Bit duration
+Nb = length(d);         % Number of bits
+
+t = 0 : 1/Fs : Nb*Tb - 1/Fs;  % Total time vector
+dt = repelem(d, Fs*Tb);       % Repeat each bit to match time vector
+
+c = A * cos(2 * pi * Fc * t); % Carrier signal
+
+PSK = zeros(1, length(t));
+for i = 1:Nb
+    idx = (i-1)*Fs*Tb + 1 : i*Fs*Tb;
+    if d(i) == 1
+        PSK(idx) = A * cos(2*pi*Fc*t(idx));
+    else
+        PSK(idx) = -A * cos(2*pi*Fc*t(idx));
+    end
+end             
+
+subplot(311)
+plot(t, dt)
+title("Digital Input Signal (Modulating signal)")
+xlabel("Time (s)")
+ylabel("Amplitude")
+axis([0 max(t) -0.5 1.5])
+
+subplot(312)
+plot(t, c)
+title("Carrier Signal")
+xlabel("Time (s)")
+ylabel("Amplitude")
+
+subplot(313)
+plot(t, PSK)
+title("Phase Shift Keying Signal (BPSK Output)")
+xlabel("Time (s)")
+ylabel("Amplitude")
